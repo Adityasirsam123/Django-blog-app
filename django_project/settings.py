4d1100de668390f7bbe7aa41138f3c7bf0b315b6
Adityasirsam123 <156391626+Adityasirsam123@users.noreply.github.com>
2024-10-14 22:38:30 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,17 +79,13 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blogdata1',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',  # or the IP address of your database server
-        'PORT': '3306',       # default MySQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
